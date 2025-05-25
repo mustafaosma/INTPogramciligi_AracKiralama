fc8f41b300e512d34e697da6eee57c97604ee13c
mustafaosma <mustafaosma024@gmail.com>
2025-05-25 13:31:26 +0300

    app.py

--- final_projesi/app.py
+++ final_projesi/app.py
@@ -309,5 +309,12 @@ def kiralama_sil(id):
     flash("Kiralama kaydı silindi", "success")
     return redirect(url_for("admin"))
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == '__main__':
+#    with app.app_context():
+#        db.create_all()
+#    app.run(debug=True)
+
+
+import os
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
